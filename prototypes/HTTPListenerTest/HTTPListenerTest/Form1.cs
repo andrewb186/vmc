13c5d7ac796e631aaf1caba64c66aa0eccc8d3bd
andrewb186 <andrewb186@gmail.com>
2014-10-05 11:15:11 +0200

    http listener prototype

---
+++ prototypes/HTTPListenerTest/HTTPListenerTest/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HTTPListenerTest
+{
+    public partial class Form1 : Form
+    {
+
+        private HttpServer httpServer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            httpServer = new HttpServer();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            httpServer.start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            httpServer.stop();
+        }
+
+        private void btnReply_Click(object sender, EventArgs e)
+        {
+            httpServer.sendResponse();
+        }
+
+       
+    }
+}
---
+++ prototypes/HTTPListenerTest/HTTPListenerTest/HttpServer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace HTTPListenerTest
+{
+    class HttpServer
+    {
+
+        private HttpListener httpListener;
+
+        public HttpServer() :this(@"http://192.168.0.25:8081")
+        {
+            
+        }
+
+        public HttpServer(string prefix)
+        {
+            Uri uri = new Uri(prefix);
+            
+            httpListener = new HttpListener();
+            httpListener.Prefixes.Add(uri.AbsoluteUri);
+        }
+
+
+        public void start()
+        {
+            httpListener.Start();
+        }
+
+
+        public void stop()
+        {
+            httpListener.Stop();
+        }
+
+
+        public void sendResponse()
+        {
+            try
+            {
+                HttpListenerContext context = httpListener.GetContext();
+                context.Response.ContentLength64 = Convert.ToInt64(Encoding.UTF8.GetByteCount(getResponseMessage()));
+                context.Response.ContentEncoding = Encoding.UTF8;
+                context.Response.ContentType = "text/plain";
+                context.Response.StatusCode = (int)HttpStatusCode.OK;
+
+                Stream stream = context.Response.OutputStream;
+                StreamWriter writer = new StreamWriter(stream);
+                writer.Write(getResponseMessage());
+                writer.Flush();
+                writer.Close();
+
+            }
+            catch (ObjectDisposedException ode)
+            {
+                //Log Exception
+            }
+            catch (ArgumentNullException ane)
+            {
+                //log
+            }
+            catch (ArgumentException ae)
+            {
+                //log
+            }
+            
+
+        }
+
+
+        private string getResponseMessage()
+        {
+            return "Hello World - Message from Http Listener - " + randomString();
+        }
+
+
+        private string randomString()
+        {
+            Random r = new Random();
+            int i = r.Next(100, 99999999);
+            return Convert.ToString(i).PadLeft(8, '0');
+        }
+
+    }
+}
