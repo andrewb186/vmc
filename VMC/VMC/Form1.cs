4dd2d6193cef359b4ba54eb36e344d69eb59ffc5
andrewb186 <andrewb186@gmail.com>
2014-10-20 21:28:31 +0200

    Added log4net logger to project

---
+++ VMC/VMC/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using log4net;
+
+
+namespace VMC
+{
+    public partial class Form1 : Form
+    {
+
+        private readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
